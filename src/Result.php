5e898674da083546d28a75289d5b4185d6672138
fmizzell <fmizzell.dev@gmail.com>
2019-07-10 11:37:57 -0500

    Initial implementation of runnable jobs.

---
+++ src/Result.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace Procrastinator;
+
+
+class Result
+{
+  const STOPPED = 'stopped';
+  const IN_PROGRESS ='in_progress';
+  const ERROR = 'error';
+  const DONE = 'done';
+
+  private $status = self::STOPPED;
+  private $data = "";
+  private $error = null;
+
+  public function setStatus($status) {
+    $statuss = [self::STOPPED, self::IN_PROGRESS, self::ERROR, self::DONE];
+    if (in_array($status, $statuss)) {
+      $this->status = $status;
+    }
+    else {
+      throw new \Exception("Invalid status {$status}");
+    }
+  }
+
+  public function setData(string $data) {
+    $this->data = $data;
+  }
+
+  public function setError(string $error) {
+    $this->error = $error;
+  }
+
+  public function getStatus() {
+    return $this->status;
+  }
+
+  public function getData() {
+    return $this->data;
+  }
+
+  public function getError() {
+    return $this->error;
+  }
+}
