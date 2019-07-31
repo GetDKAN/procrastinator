f2c3ca03517246e998d344d9ddd63677c473ad45
Dan Feder <dafeder@gmail.com>
2019-07-31 09:20:08 -0500

    PSR-4 fixes

--- src/Hydratable.php
+++ src/Hydratable.php
@@ -3,21 +3,21 @@
 
 namespace Procrastinator;
 
-
 trait Hydratable
 {
-  public static function hydrate($json) {
-    $data = json_decode($json);
+    public static function hydrate($json)
+    {
+        $data = json_decode($json);
 
-    $reflector = new \ReflectionClass(self::class);
-    $object = $reflector->newInstanceWithoutConstructor();
+        $reflector = new \ReflectionClass(self::class);
+        $object = $reflector->newInstanceWithoutConstructor();
 
-    $reflector = new \ReflectionClass($object);
-    foreach ($data as $property => $value) {
-      $p = $reflector->getProperty($property);
-      $p->setAccessible(true);
-      $p->setValue($object, $value);
+        $reflector = new \ReflectionClass($object);
+        foreach ($data as $property => $value) {
+            $p = $reflector->getProperty($property);
+            $p->setAccessible(true);
+            $p->setValue($object, $value);
+        }
+        return $object;
     }
-    return $object;
-  }
 }
--- src/Job/Job.php
+++ src/Job/Job.php
@@ -7,102 +7,104 @@ use Procrastinator\Result;
 
 abstract class Job implements \JsonSerializable
 {
-  private $result;
-  private $timeLimit;
+    private $result;
+    private $timeLimit;
 
-  public function __construct()
-  {
-    $this->result = new Result(Result::STOPPED);
-  }
-
-  public function run(): Result
-  {
-    $this->result->setStatus(Result::IN_PROGRESS);
-
-    try {
-      $data = $this->runIt();
-    }
-    catch (\Exception $e) {
-      $this->result->setStatus(Result::ERROR);
-      $this->result->setError($e->getMessage());
-      return $this->result;
+    public function __construct()
+    {
+        $this->result = new Result(Result::STOPPED);
     }
 
-    if ($data) {
-      if ($data instanceof Result) {
-        $this->result = $data;
-      }
-      else if (is_string($data)) {
-        $this->result->setData($data);
-        $this->result->setStatus(Result::DONE);
-      }
-      else {
-        throw new \Exception("Invalid result or data format.");
-      }
+    public function run(): Result
+    {
+        $this->result->setStatus(Result::IN_PROGRESS);
+
+        try {
+            $data = $this->runIt();
+        } catch (\Exception $e) {
+            $this->result->setStatus(Result::ERROR);
+            $this->result->setError($e->getMessage());
+            return $this->result;
+        }
+
+        if ($data) {
+            if ($data instanceof Result) {
+                $this->result = $data;
+            } elseif (is_string($data)) {
+                $this->result->setData($data);
+                $this->result->setStatus(Result::DONE);
+            } else {
+                throw new \Exception("Invalid result or data format.");
+            }
+        } else {
+            $this->result->setStatus(Result::DONE);
+        }
+
+        return $this->result;
     }
-    else {
-      $this->result->setStatus(Result::DONE);
+
+    abstract protected function runIt();
+
+    public function setTimeLimit(int $seconds)
+    {
+        $this->timeLimit = $seconds;
     }
 
-    return $this->result;
-  }
+    public function unsetTimeLimit()
+    {
+        unset($this->timeLimit);
+    }
 
-  abstract protected function runIt();
+    public function getState()
+    {
+        return (array) json_decode($this->getResult()->getData());
+    }
 
-  public function setTimeLimit(int $seconds)
-  {
-    $this->timeLimit = $seconds;
-  }
+    public function getStateProperty($property)
+    {
+        return $this->getState()[$property];
+    }
 
-  public function unsetTimeLimit() {
-    unset($this->timeLimit);
-  }
+    public function getResult(): Result
+    {
+        return $this->result;
+    }
 
-  public function getState() {
-    return (array) json_decode($this->getResult()->getData());
-  }
+    private function setState($state)
+    {
+        $this->getResult()->setData(json_encode($state));
+    }
 
-  public function getStateProperty($property) {
-    return $this->getState()[$property];
-  }
+    public function setStateProperty($property, $value)
+    {
+        $state = $this->getState();
+        $state[$property] = $value;
+        $this->setState($state);
+    }
 
-  public function getResult(): Result
-  {
-    return $this->result;
-  }
+    public function jsonSerialize()
+    {
+        return (object) ['timeLimit' => $this->timeLimit, 'result' => $this->getResult()];
+    }
 
-  private function setState($state) {
-    $this->getResult()->setData(json_encode($state));
-  }
+    public static function hydrate($json)
+    {
+        $data = json_decode($json);
 
-  public function setStateProperty($property, $value) {
-    $state = $this->getState();
-    $state[$property] = $value;
-    $this->setState($state);
-  }
+        $reflector = new \ReflectionClass(self::class);
+        $object = $reflector->newInstanceWithoutConstructor();
 
-  public function jsonSerialize()
-  {
-    return (object) ['timeLimit' => $this->timeLimit, 'result' => $this->getResult()];
-  }
+        $reflector = new \ReflectionClass($object);
 
-  public static function hydrate($json) {
-    $data = json_decode($json);
+        $p = $reflector->getProperty('timeLimit');
+        $p->setAccessible(true);
+        $p->setValue($object, $data->timeLimit);
 
-    $reflector = new \ReflectionClass(self::class);
-    $object = $reflector->newInstanceWithoutConstructor();
+        $class = $reflector->getParentClass();
+        $p = $class->getProperty('result');
+        $p->setAccessible(true);
+        $p->setValue($object, Result::hydrate(json_encode($data->result)));
 
-    $reflector = new \ReflectionClass($object);
-
-    $p = $reflector->getProperty('timeLimit');
-    $p->setAccessible(true);
-    $p->setValue($object, $data->timeLimit);
-
-    $class = $reflector->getParentClass();
-    $p = $class->getProperty('result');
-    $p->setAccessible(true);
-    $p->setValue($object, Result::hydrate(json_encode($data->result)));
-
-    return $object;
-  }
+        return $object;
+    }
 }
