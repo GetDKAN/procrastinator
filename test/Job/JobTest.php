178deaf3dcceff23a171b7c279b69cade40aecaa
fmizzell <fmizzell.dev@gmail.com>
2019-10-17 16:23:42 -0500

    Creating mock job.

--- test/Job/JobTest.php
+++ test/Job/JobTest.php
@@ -2,11 +2,12 @@
 
 namespace ProcrastinatorTest\Job;
 
-use Procrastinator\Job\Job;
+use PHPUnit\Framework\TestCase;
 use Procrastinator\Job\Method;
 use Procrastinator\Result;
+use ProcrastinatorTest\Job\Mock\TwoStage;
 
-class JobTest extends \PHPUnit\Framework\TestCase
+class JobTest extends TestCase
 {
     public function test()
     {
@@ -80,29 +81,5 @@ class JobTest extends \PHPUnit\Framework\TestCase
     public function callReturn()
     {
         return "Hello";
-    }
-}
-
-class TwoStage extends Job
-{
-    private $stage = 1;
-
-    protected function runIt()
-    {
-        if ($this->stage == 1) {
-            $this->stage = 2;
-
-            $result = $this->getResult();
-            $result->setStatus(Result::STOPPED);
-            $result->setData(json_encode(['a', 'b', 'c']));
-
-            return $result;
-        } elseif ($this->stage == 2) {
-            $data_string = $this->getResult()->getData();
-            $data = json_decode($data_string);
-            $data[] = 'd';
-
-            return json_encode($data);
-        }
     }
 }
