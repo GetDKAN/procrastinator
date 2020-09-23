df35c509578a792054a7afb4ef184f638f6a1b41
Dan Feder <dan.feder@civicactions.com>
2020-09-22 20:27:47 -0400

    Throw exception if invalid JSON in hydrate

--- src/HydratableTrait.php
+++ src/HydratableTrait.php
@@ -8,6 +8,10 @@ trait HydratableTrait
     {
         $data = (array) json_decode($json);
 
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \Exception(json_last_error_msg());
+        }
+
         $class = new \ReflectionClass(static::class);
 
         if (!$instance) {
