d3f205728cef64f62e692521cd8b5d18d4039ece
Paul Mitchum <paul@mile23.com>
2024-05-07 11:05:35 -0700

    updated docblocks

--- src/HydratableInterface.php
+++ src/HydratableInterface.php
@@ -2,7 +2,27 @@
 
 namespace Procrastinator;
 
+/**
+ * Interface to hydrate JSON into PHP variables.
+ *
+ * This interface is similar to \JsonSerializable in that we can 'hydrate' JSON
+ * into PHP structures.
+ *
+ * @see \Procrastinator\HydratableTrait
+ * @see \JsonSerializable
+ */
 interface HydratableInterface extends \JsonSerializable
 {
+    /**
+     * Hydrate some JSON into a PHP object or array or other variable.
+     *
+     * @param string $json
+     *   The JSON to process.
+     * @param $instance
+     *   (Optional) Create a new instance without invoking the constructor.
+     *
+     * @return mixed
+     *   The hydrated data structure.
+     */
     public static function hydrate(string $json, $instance = null);
 }
